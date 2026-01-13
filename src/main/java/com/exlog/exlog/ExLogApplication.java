d2fe4f72cf446ede0dc40faf6525b0f3db00fd38
DongWook-Kwak <kdw381@naver.com>
2026-01-13 22:13:41 +0900

    chore : initial project setup

---
+++ src/main/java/com/exlog/exlog/ExLogApplication.java
@@ -0,0 +1,13 @@
+package com.exlog.exlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exlog/exlog/ExLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exlog.exlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
