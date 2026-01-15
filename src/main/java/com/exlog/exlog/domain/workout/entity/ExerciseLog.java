d26811539073b9485177cbddf3644ab7363a5f4d
DongWook-Kwak <kdw381@naver.com>
2026-01-15 23:51:49 +0900

    feat : 운동 기록 entity

---
+++ src/main/java/com/exlog/exlog/domain/workout/entity/ExerciseLog.java
@@ -0,0 +1,50 @@
+package com.exlog.exlog.domain.workout.entity;
+
+import com.exlog.exlog.domain.auth.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ExerciseLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "log_id")
+    private Long logId;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Exercise exercise;
+
+    @Column(nullable = false)
+    private Long reps;
+
+    @Column(nullable = false)
+    private Long sets;
+
+    @Column(name = "create_at", nullable = false)
+    private LocalDateTime createAt;
+
+
+
+}
