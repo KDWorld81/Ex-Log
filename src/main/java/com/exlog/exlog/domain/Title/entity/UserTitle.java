340c342b844d4fd689fbf83c6424454ebbf553cc
DongWook-Kwak <kdw381@naver.com>
2026-01-16 00:17:34 +0900

    feat : 칭호와 User N:M 해결할 중간 entity

---
+++ src/main/java/com/exlog/exlog/domain/Title/entity/UserTitle.java
@@ -0,0 +1,42 @@
+package com.exlog.exlog.domain.Title.entity;
+
+import com.exlog.exlog.domain.auth.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class UserTitle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "usertitle_id", nullable = false)
+    private Long usertitleId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private Title title;
+
+    @Column(name = "acquired_at", nullable = false)
+    private LocalDateTime acquiredAt;
+}
