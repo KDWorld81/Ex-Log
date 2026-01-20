7582c576f87f489d8ed625f3d1b66688f5c795ca
DongWook-Kwak <kdw381@naver.com>
2026-01-20 22:31:40 +0900

    feat : 토큰 필드 추가

--- src/main/java/com/exlog/exlog/domain/auth/dto/LoginResDto.java
+++ src/main/java/com/exlog/exlog/domain/auth/dto/LoginResDto.java
@@ -1,5 +1,6 @@
 package com.exlog.exlog.domain.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,6 +14,13 @@ import lombok.NoArgsConstructor;
 
 // 로그인 후 메인화면 보일때 필요한
 public class LoginResDto {
+
+    @JsonIgnore // 컨트롤러에서는 꺼낼수 있으나 JSON응답에는 포함되지 않도록
+    private String accessToken;
+
+    @JsonIgnore
+    private String refreshToken;
+
     private String username;
 
 }
