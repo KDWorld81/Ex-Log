4ec9dd1d8ecab99ee45aa6d5dd977f7d1101e65e
DongWook-Kwak <kdw381@naver.com>
2026-01-23 15:17:22 +0900

    refact : 패키지 재분리

--- src/main/java/com/exlog/exlog/pagecontroller/authpage/AuthPageController.java
+++ src/main/java/com/exlog/exlog/pagecontroller/authpage/AuthPageController.java
@@ -1,4 +1,4 @@
-package com.exlog.exlog.global.authpage;
+package com.exlog.exlog.pagecontroller.authpage;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
