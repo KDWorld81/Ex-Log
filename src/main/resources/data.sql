b4fbd53ebc3c66615d4640b2c5b9622308c1ea11
DongWook-Kwak <kdw381@naver.com>
2026-01-24 19:39:08 +0900

    feat : 카테고리 기본 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT IGNORE INTO category (category_id, category_name) VALUES (1, '가슴');
+INSERT IGNORE INTO category (category_id, category_name) VALUES (2, '등');
+INSERT IGNORE INTO category (category_id, category_name) VALUES (3, '하체');
+INSERT IGNORE INTO category (category_id, category_name) VALUES (4, '어깨');
+INSERT IGNORE INTO category (category_id, category_name) VALUES (5, '팔');
