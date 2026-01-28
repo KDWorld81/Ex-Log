4199e324c0ed392547af1673a23fae8df7ab0566
DongWook-Kwak <kdw381@naver.com>
2026-01-28 23:16:25 +0900

    feat : fromEntity 메소드 추가

--- src/main/java/com/exlog/exlog/domain/exercise/dto/CategoryResDto.java
+++ src/main/java/com/exlog/exlog/domain/exercise/dto/CategoryResDto.java
@@ -1,12 +1,23 @@
 package com.exlog.exlog.domain.exercise.dto;
 
+import com.exlog.exlog.domain.exercise.entity.Category;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
+@Builder
 @RequiredArgsConstructor
 public class CategoryResDto {
 
     private final Long categoryId;
     private final String categoryName;
+
+    public static CategoryResDto fromEntity(Category category) {
+        return CategoryResDto.builder()
+                .categoryId(category.getCategoryId())
+                .categoryName(category.getCategoryName())
+                .build();
+
+    }
 }
