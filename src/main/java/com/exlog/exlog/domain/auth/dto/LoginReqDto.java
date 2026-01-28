7013cb8e3cb302d1797378b9eecefd60538f8f0e
DongWook-Kwak <kdw381@naver.com>
2026-01-28 21:01:29 +0900

    feat : builder 어노테이션 추가

--- src/main/java/com/exlog/exlog/domain/auth/dto/LoginReqDto.java
+++ src/main/java/com/exlog/exlog/domain/auth/dto/LoginReqDto.java
@@ -1,10 +1,12 @@
 package com.exlog.exlog.domain.auth.dto;
 
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
+@Builder
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 
 public class LoginReqDto {
