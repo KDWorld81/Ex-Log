5c4b7391c3a694e00d2ae1985926b8d11b795e4b
DongWook-Kwak <kdw381@naver.com>
2026-01-28 19:18:42 +0900

    feat : Dto에서 toEntity 구현

--- src/main/java/com/exlog/exlog/domain/auth/dto/SignupReqDto.java
+++ src/main/java/com/exlog/exlog/domain/auth/dto/SignupReqDto.java
@@ -1,13 +1,18 @@
 package com.exlog.exlog.domain.auth.dto;
 
+import com.exlog.exlog.domain.auth.entity.Tier;
+import com.exlog.exlog.domain.auth.entity.User;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
+@Builder
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 
 public class SignupReqDto {
@@ -24,4 +29,15 @@ public class SignupReqDto {
 
     @NotBlank(message = "성별을 선택해주세요.")
     private final String gender;
+
+    public User toEntity(PasswordEncoder passwordEncoder) {
+        return User.builder()
+                .email(this.getEmail())
+                .password(passwordEncoder.encode(this.password))
+                .username(this.username)
+                .gender(this.gender)
+                .totalExp(0L)
+                .tier(Tier.BRONZE)
+                .build();
+    }
 }
