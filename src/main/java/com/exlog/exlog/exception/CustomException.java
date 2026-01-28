47e5500dda235a52f06c3824c92f8584e5c4a29c
DongWook-Kwak <kdw381@naver.com>
2026-01-28 20:57:40 +0900

    feat : Errorcode enum 기반 커스텀 예외클래스 추가

---
+++ src/main/java/com/exlog/exlog/exception/CustomException.java
@@ -0,0 +1,10 @@
+package com.exlog.exlog.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
