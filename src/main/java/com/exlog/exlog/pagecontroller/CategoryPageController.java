fe0cee58ab4aace398b98473ada850e87815c944
DongWook-Kwak <kdw381@naver.com>
2026-01-29 23:35:46 +0900

    fix : 페이지만 리다이렉트 하는것으로 복구

--- src/main/java/com/exlog/exlog/pagecontroller/CategoryPageController.java
+++ src/main/java/com/exlog/exlog/pagecontroller/CategoryPageController.java
@@ -1,20 +1,11 @@
 package com.exlog.exlog.pagecontroller;
 
-import com.exlog.exlog.domain.exercise.service.CategoryService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-@RequiredArgsConstructor
 public class CategoryPageController {
 
-    private final CategoryService categoryService;
-
     @GetMapping("/category")
-    public String categoryPage(Model model) {
-        model.addAttribute("categories", categoryService.findAllCategories());
-        return "exercise/category";
-    }
+    public String categoryPage() {return "exercise/category";}
 }
