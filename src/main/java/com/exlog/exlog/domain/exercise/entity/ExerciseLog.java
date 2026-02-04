45463748d0dbb8c8b653f36c90bfd677978b6c1f
DongWook-Kwak <kdw381@naver.com>
2026-02-05 01:21:32 +0900

    feat: JPA Auditing 기능을 활용한 생성 시간 자동화

--- src/main/java/com/exlog/exlog/domain/exercise/entity/ExerciseLog.java
+++ src/main/java/com/exlog/exlog/domain/exercise/entity/ExerciseLog.java
@@ -3,6 +3,7 @@ package com.exlog.exlog.domain.exercise.entity;
 import com.exlog.exlog.domain.auth.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,6 +15,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
@@ -22,6 +25,7 @@ import java.time.LocalDateTime;
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class ExerciseLog {
 
     @Id
@@ -43,6 +47,7 @@ public class ExerciseLog {
     @Column(nullable = false)
     private Long sets;
 
+    @CreatedDate
     @Column(name = "create_at", nullable = false)
     private LocalDateTime createAt;
 
