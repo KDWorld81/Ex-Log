c960b769b34693e5ebc41ad970568dd003ba8425
DongWook-Kwak <kdw381@naver.com>
2026-02-04 23:14:49 +0900

    feat : 운동기록 Dto 구현

---
+++ src/main/java/com/exlog/exlog/domain/exercise/dto/ExerciseLogReqDto.java
@@ -0,0 +1,37 @@
+package com.exlog.exlog.domain.exercise.dto;
+
+import com.exlog.exlog.domain.auth.entity.User;
+import com.exlog.exlog.domain.exercise.entity.Exercise;
+import com.exlog.exlog.domain.exercise.entity.ExerciseLog;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@Builder
+public class ExerciseLogReqDto {
+
+    @NotNull
+    private final Long exerciseId;
+
+    @NotNull
+    private final Long userId;
+
+    @NotNull
+    private final Long reps;
+
+    @NotNull
+    private final Long sets;
+
+    public ExerciseLog toEntity(User user, Exercise exercise) {
+        return ExerciseLog.builder()
+                .exercise(exercise)
+                .user(user)
+                .reps(this.reps)
+                .sets(this.sets)
+                .build();
+    }
+
+}
