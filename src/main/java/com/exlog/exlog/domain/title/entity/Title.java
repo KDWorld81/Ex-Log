aed7d46ded02cc176ac42998106b0730aa730990
DongWook-Kwak <kdw381@naver.com>
2026-02-10 23:02:02 +0900

    refact : 패키지명 변경

--- src/main/java/com/exlog/exlog/domain/title/entity/Title.java
+++ src/main/java/com/exlog/exlog/domain/title/entity/Title.java
@@ -1,8 +1,11 @@
-package com.exlog.exlog.domain.Title.entity;
+package com.exlog.exlog.domain.title.entity;
 
-import com.exlog.exlog.domain.auth.entity.User;
+import com.exlog.exlog.domain.title.TitleCondition;
+import com.exlog.exlog.domain.exercise.entity.Category;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,28 +18,30 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class UserTitle {
+public class Title {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "usertitle_id", nullable = false)
-    private Long usertitleId;
+    @Column(name = "title_id")
+    private Long titleId;
+
+    @Column(name = "title_name", nullable = false)
+    private String titleName;
+
+    @Column(nullable = false)
+    private String explanation;
+
+    @Enumerated(EnumType.STRING)
+    private TitleCondition titleCondition;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(nullable = false)
-    private User user;
+    @JoinColumn(name = "category_id")
+    private Category category;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(nullable = false)
-    private Title title;
-
-    @Column(name = "acquired_at", nullable = false)
-    private LocalDateTime acquiredAt;
+    private Long threshold;
 }
--- src/main/java/com/exlog/exlog/domain/title/entity/UserTitle.java
+++ src/main/java/com/exlog/exlog/domain/title/entity/UserTitle.java
@@ -1,11 +1,8 @@
-package com.exlog.exlog.domain.Title.entity;
+package com.exlog.exlog.domain.title.entity;
 
-import com.exlog.exlog.domain.Title.TitleCondition;
-import com.exlog.exlog.domain.exercise.entity.Category;
+import com.exlog.exlog.domain.auth.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,30 +15,28 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Title {
+public class UserTitle {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "title_id")
-    private Long titleId;
-
-    @Column(name = "title_name", nullable = false)
-    private String titleName;
-
-    @Column(nullable = false)
-    private String explanation;
-
-    @Enumerated(EnumType.STRING)
-    private TitleCondition titleCondition;
+    @Column(name = "usertitle_id", nullable = false)
+    private Long usertitleId;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "category_id")
-    private Category category;
+    @JoinColumn(nullable = false)
+    private User user;
 
-    private Long threshold;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(nullable = false)
+    private Title title;
+
+    @Column(name = "acquired_at", nullable = false)
+    private LocalDateTime acquiredAt;
 }
