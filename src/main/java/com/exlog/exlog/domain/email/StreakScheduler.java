f8f5fedec5abb21f26cf9857afc573d07933a56d
DongWook-Kwak <kdw381@naver.com>
2026-02-27 00:17:40 +0900

    feat : StreakScheduler 구현

---
+++ src/main/java/com/exlog/exlog/domain/email/StreakScheduler.java
@@ -0,0 +1,26 @@
+package com.exlog.exlog.domain.email;
+
+import com.exlog.exlog.domain.auth.entity.User;
+import com.exlog.exlog.domain.auth.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class StreakScheduler {
+
+    private final UserRepository userRepository;
+    private final EmailService emailService;
+
+    @Scheduled(cron = "0 0 9 * * *") // 매일
+    public void sendWarningEmails() {
+        LocalDate threeDaysAgo = LocalDate.now().minusDays(3);
+        List<User> targetUsers = userRepository.findUsersByLastExerciseDate(threeDaysAgo);
+
+        targetUsers.forEach(emailService::sendStreakWarningEmail);
+    }
+}
